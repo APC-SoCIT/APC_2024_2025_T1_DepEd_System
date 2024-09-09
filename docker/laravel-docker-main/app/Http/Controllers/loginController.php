a67fe8e8a13c5cf7539fa310f7123b3dc1b79df5
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2024-09-09 16:32:12 +0800

    Edited registerTeacher function in loginController.php

--- docker/laravel-docker-main/app/Http/Controllers/loginController.php
+++ docker/laravel-docker-main/app/Http/Controllers/loginController.php
@@ -24,17 +24,38 @@ class loginController extends Controller
     function register(){
         return view('register');
     }
-    function registerPost(Request $request){
+    function registerError(){
+        return view('register?error');
+    }
+    function registerTeacher(Request $request){
         $request->validate([
-            'username'=>'required',
-            'password'=>'required'
+            'fname'=>'required',
+            'lname'=>'required',
+            'sname'=>'required',
+            'sid'=>'required',
+            'district'=>'required',
+            'email'=>'required',
+            'password'=>'required|confirmed',
+            'cpassword'=>'required',
         ]);
-
-        $data['username'] = $request->username;
-        $data['password'] = Hash::make($request->password);
-
+    
+        if ($request->password !== $request->cpassword) {
+            return redirect()->route('register', http_build_query(array_merge(\Request::query(), ['error' => true])));
+        }
+    
+        $data = [
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'sname' => $request->sname,
+            'sid' => $request->sid,
+            'district' => $request->district,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+    
+        // Must Edit this and Models/User.php
         $user = User::create($data);
-
-        return redirect()->route('login');
+    
+        return redirect()->route('login-teacher', http_build_query(array_merge(\Request::query(), ['registered' => true])));
     }
 }
