293c7b94bc895bf2867bf5ef7fa5f8f6c9f1121a
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2024-09-09 16:15:44 +0800

    Completed registration page, added register.css

--- docker/laravel-docker-main/public/js/login.js
+++ docker/laravel-docker-main/public/js/login.js
@@ -8,6 +8,13 @@ function invalid_credentials(){
 function registered(){
     Swal.fire({
         icon: "success",
-        title: "Registered Successfully",
+        title: "Registered Successfully!",
       });
 }
+function register_error(){
+    Swal.fire({
+        icon: "error",
+        title: "Registration Error",
+        text: "Please try again.",
+      });
+};
--- docker/laravel-docker-main/resources/views/register.blade.php
+++ docker/laravel-docker-main/resources/views/register.blade.php
@@ -1,1 +1,79 @@
-This is registration page
+@extends('layouts.master-layout')
+@push('css')
+    <link href="{{asset('css/register.css')}}" rel="stylesheet">
+@endpush
+
+@section('content')
+<body>
+
+<div id="shadow">
+</div>
+
+<div id="centered-container">
+    <center>
+        <img id="logo" src="{{ asset('DepEd_logo.ico') }}">
+        <p id='heading'><b>DepED Teacher Portal</b></p>
+        <p id='subheading'>Department of Education Record System</p>
+        <form id="login" action="" method="POST">
+        @csrf
+        <div id="container">
+            <div class="row">
+                <div class="mb-3 col-sm">
+                    <label class="form-label">First Name</label>
+                    <input type="text" class="form-control" id="fname" name="fname" required>
+                </div>
+                <div class="mb-3 col-sm">
+                    <label class="form-label">Last Name</label>
+                    <input type="text" class="form-control" id="lname" name="lname" required>
+                </div>
+            </div>
+        <div class="mb-3">
+            <label class="form-label">School Name</label>
+            <input type="text" class="form-control" id="sname" name="sname" required>
+        </div>
+            <div class="row">
+                <div class="mb-3 col-sm">
+                    <label class="form-label">School ID</label>
+                    <input type="text" class="form-control" id="sid" name="sid" required>
+                </div>
+                <div class="mb-3 col-sm">
+                    <label class="form-label">District</label>
+                    <input type="text" class="form-control" id="district" name="district" required>
+                </div>
+            </div>
+        <div class="mb-3">
+            <label class="form-label">E-mail</label>
+            <input type="text" class="form-control" id="email" name="email" required>
+        </div>
+            <div class="row">
+                <div class="mb-3 col-sm">
+                    <label class="form-label">Password</label>
+                    <input type="text" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="mb-3 col-sm">
+                    <label class="form-label">Confirm Password</label>
+                    <input type="text" class="form-control" id="cpassword" name="cpassword" required>
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 512 512">
+                <style>svg{fill:#ffffff}</style>
+                <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/>
+            </svg>
+            Register
+        </button>
+        </form>
+        <?php
+         if(isset($_GET['error'])){
+            echo '<script>window.onload = function() { register_error(); }</script>';
+        }
+        ?>
+    </center>
+</div>
+</body>
+@endsection
+@push('script')
+    <script src="{{ asset('js/login.js')}}"></script>
+@endpush
+
