95feeee3a1bc459efebca9734a460660b145c94e
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2024-11-08 16:55:16 +0000

    added more secure_asset

--- docker/laravel-docker-main/resources/views/layouts/dashboard-layout.blade.php
+++ docker/laravel-docker-main/resources/views/layouts/dashboard-layout.blade.php
@@ -8,16 +8,16 @@
     <title>DepEd Portal</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
-    <link href="{{asset('css/dashboard.css')}}" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="{{ asset('DepED_logo.ico') }}">
+    <link href="{{secure_asset('css/dashboard.css')}}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('DepED_logo.ico') }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @stack('css')
 </head>
 @yield('content')
 </html>   
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ asset('js/aos.js')}}"></script>
+    <script src="{{ secure_asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ secure_asset('js/aos.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.18/sweetalert2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- docker/laravel-docker-main/resources/views/layouts/master-layout.blade.php
+++ docker/laravel-docker-main/resources/views/layouts/master-layout.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DepEd Portal</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.18/sweetalert2.min.css">
-    <link rel="icon" type="image/x-icon" href="{{ asset('DepED_logo.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('DepED_logo.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @stack('css')
     @yield('content')
     
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ asset('js/aos.js')}}"></script>
+    <script src="{{ secure_asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ secure_asset('js/aos.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.18/sweetalert2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
